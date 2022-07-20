c2f3f1ce56b5116a55b0e0ea69e317e75ee6bbbf
Dejhon <dejhonsherman6@gmail.com>
2022-07-19 19:07:00 -0500

    updates

--- src/app/Services/students.service.ts
+++ src/app/Services/students.service.ts
@@ -49,7 +49,7 @@ export class StudentsService {
 
   // Delete Student By Id
   deleteStudent(id:string):Observable<Students>{
-    return this.http.delete<Students>(`${this.REST_API_URL}/remove/${id}`).pipe(
+    return this.http.get<Students>(`${this.REST_API_URL}/remove/${id}`).pipe(
       tap( removedStudent => console.log(`This Record Was Deleted: ${JSON.stringify(removedStudent)}`)),
       catchError( error => of())
     )
